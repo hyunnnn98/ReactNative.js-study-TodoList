0ba1dd4669a025da63739df8428530d14daed0ac
SeungHyun Cho <c0305sh@naver.com>
2020-10-09 04:09:11 +0900

    Feature :: Setting Up and Displaying Lists

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import HelloWorld from './components/HelloWorld';
-import CustomScrollView from './components/CustomScrollView';
-import CustomFlatList from './components/CustomFlatList';
+import { StyleSheet, Text, TouchableOpacity, View, FlatList } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import tempData from './tempData';
+import TodoList from './components/TodoList';
+import Colors from './Colors';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      {/* <HelloWorld /> */}
-      <CustomFlatList/>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo <Text style={{ fontWeight: "500", color: Colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={Colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+        </View>
+
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={item => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <TodoList list={item} />
+            )}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -23,4 +48,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: Colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center"
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: Colors.black,
+    paddingHorizontal: 64
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: Colors.lightBlue,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  add: {
+    color: Colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8
+  }
 });
---
+++ tempData.js
@@ -0,0 +1,74 @@
+// Temporary Data => 파이어베이스 연동 전 까지 사용.
+
+export default tempData = [
+    {
+        name: "일본 여행 계획",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "여행 가이드북 챙기기",
+                completed: true
+            },
+            {
+                title: "여권 확인",
+                completed: true
+            },
+            {
+                title: "호텔 예약 확인",
+                completed: true
+            },
+            {
+                title: "수화물 확인",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "농협 마트가기",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "초코파이 큰거",
+                completed: false
+            },
+            {
+                title: "쓰레기 봉투",
+                completed: true
+            },
+            {
+                title: "우유",
+                completed: true
+            },
+            {
+                title: "설탕",
+                completed: false
+            },
+            {
+                title: "컵라면",
+                completed: false
+            },
+        ]
+    },
+    {
+        name: "캡스톤 프로젝트",
+        color: "orange",
+        todos: [
+            {
+                title: "UML 그리기",
+                completed: true
+            },
+            {
+                title: "스토리 보드 그리기",
+                completed: true
+            },
+            {
+                title: "개발 역할 분담하기",
+                completed: false
+            },
+            {
+                title: "개발 언어 정하기",
+                completed: false
+            },
+        ]
+    },
+]
